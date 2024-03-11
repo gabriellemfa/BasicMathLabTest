718bd8f54b156a14f266360feef0433f0452815f
amxgielle <152216187+amxgielle@users.noreply.github.com>
2024-03-11 00:23:20 -0600

    Add project files.

---
+++ BasicMathLabTest.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BasicMathLab; 
+
+//using Unit Test intead of NUnit Framework
+
+
+namespace BasicMathLabTest
+{
+
+    //Test class for BasicMathClass
+    [TestClass]
+    public class BasicMathLabTest
+    {
+
+        //Instande of BasicMath class for the following tests
+        private BasicMath calculator;
+
+
+        //Method to set up my test environment
+        [TestInitialize]
+        public void Setup()
+        {
+            //Initialize BasicMath instance
+            calculator = new BasicMath();
+        }
+
+        //Cleans up test after
+        [TestCleanup]
+        public void Teardown()
+        {
+            calculator = null;
+        }
+
+        //Test method to test Add method of BasicMath
+        [TestMethod]
+        public void Test_AddMethod()
+        {
+            // Arrange
+            double num1 = 4;
+            double num2 = 44;
+            double expected = 48;
+
+            // Act: Performs addition
+            double result = calculator.Add(num1, num2);
+
+            // Assert: Checks if result = expected
+            Assert.AreEqual(expected, result);
+        }
+
+        //Test method to test Subtraction method of BasicMath
+        [TestMethod]
+        public void Test_SubtractMethod()
+        {
+            // Arrange
+            double num1 = 50;
+            double num2 = 20;
+            double expected = 30;
+
+            // Act: Performs subtraction
+            double result = calculator.Subtract(num1, num2);
+
+            // Assert: Checks if result = expected
+            Assert.AreEqual(expected, result);
+        }
+
+        //Test method to test Multiplication method of BasicMath
+        [TestMethod]
+        public void Test_MultiplyMethod()
+        {
+            // Arrange
+            double num1 = 5;
+            double num2 = 6;
+            double expected = 30;
+
+            // Act: Performs multiplication
+            double result = calculator.Multiply(num1, num2);
+
+            // Assert: Checks if result = expected
+            Assert.AreEqual(expected, result);
+        }
+
+        //Test method to test Dividion method of BasicMath
+        [TestMethod]
+        public void Test_DivideMethod()
+        {
+            // Arrange
+            double num1 = 10;
+            double num2 = 2;
+            double expected = 5;
+
+            // Act: Performs division
+            double result = calculator.Divide(num1, num2);
+
+            // Assert: Checks if result = expected
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BasicMathLabTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BasicMathLabTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d50d5003-ebf5-4fd0-978e-2e716b9e8a15")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
